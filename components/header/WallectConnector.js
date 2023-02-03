b941a8c256c9fd4db4138de875c6e61f550a467d
RyoLinEth <ryolindev@gmail.com>
2023-02-03 23:50:10 +0800

    check if is ether problem

--- components/header/WallectConnector.js
+++ components/header/WallectConnector.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-// import { ethers } from 'ethers'
+import { ethers } from 'ethers'
 import swal from 'sweetalert'
 
 const WalletConnect = ({ defaultAccountChange, language }) => {
